958f023f76db917c5e137d4286d42a33a4611829
Peter John Alvarado <peterjohn@gorated.ph>
2019-06-17 21:42:56 +0800

    Add prompts

--- src/prompts.js
+++ src/prompts.js
@@ -38,5 +38,60 @@
  */
 
 module.exports = function () {
-  return []
+  return [
+    {
+      type: 'checkbox',
+      name: 'environments',
+      message: "Choose supported environments",
+      choices: [
+        {
+          name: 'Development (recommended)',
+          value: 'development',
+          checked: true
+        },
+        {
+          name: 'Production (recommended)',
+          value: 'production',
+          checked: true
+        },
+        {
+          name: 'Staging',
+          value: 'staging',
+        },
+        {
+          name: 'Test',
+          value: 'test',
+        },
+        {
+          name: 'QA',
+          value: 'qa',
+        },
+      ],
+      validate: (answer) => {
+        if (answer.length < 1) {
+          return 'You must choose at least one environment';
+        }
+
+        return true;
+      }
+    },
+    {
+      type: 'input',
+      name: 'common_root_object',
+      message: "What name would you like to use for your Common Root Object ('none' means to not use one)?",
+      default: "none"
+    },
+    {
+      type: 'confirm',
+      name: 'create_env_files',
+      message: "Create your .env files for you?",
+      default: true
+    },
+    {
+      type: 'confirm',
+      name: 'add_env_to_gitignore',
+      message: "For security, would you like your .env files automatically added to .gitignore?",
+      default: true
+    }
+  ]
 }
